53201857ad9a1fb61703b0b09bf0df83f37ac947
sean23333 <93200364+sean23333@users.noreply.github.com>
2021-11-28 19:47:39 +0000

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,9 +1,17 @@
 '''
 Write a function that takes as arguments a numpy array, v and a tuple of two numbers a and b (with b >= a).
 the function will return a Numpy array that contains all the values in v that are in the interval [a,b] (notice the inclusion).
-
 example: input1 = np.array([1,2,3,4,5,6]), input2 = (1,5), output = np.array([1,2,3,4,5])
 Provide examples to showcase that the functions works.
-
 Weight: 1
 '''
+import numpy as np
+def out(v,a,b):
+    x = np.arange(int(v))
+    y = x [a:b+1:1]
+    return y
+
+v = input()
+a = int(input())
+b = int(input())
+print(out(v,a,b))
