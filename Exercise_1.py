0d30cd1786a0c7354e0f3cfc82391dacf8b07512
sean23333 <93200364+sean23333@users.noreply.github.com>
2021-12-01 21:34:03 +0000

    Update Exercise_1.py

--- Exercise_1.py
+++ Exercise_1.py
@@ -6,4 +6,16 @@ example: input1 = np.array([1,2,3,4,5,6]), input2 = (1,5), output = np.array([1,
 Provide examples to showcase that the functions works.
 
 Weight: 1
+
 '''
+
+import numpy as np
+def out(v,a,b):
+    x = np.arange(int(v))
+    y = x [a:b+1:1]
+    return y
+
+v = input()
+a = int(input())
+b = int(input())
+print(out(v,a,b))
